550832f1fc7cc94720d14c5049fa503549cc0acd
Krzysztof Gaszyński <k.gaszynski@gmail.com>
2018-08-07 17:52:47 +0200

    Adding shared emails when creating a board

--- src/components/BoardCreate/BoardCreate.js
+++ src/components/BoardCreate/BoardCreate.js
@@ -4,6 +4,7 @@ import { Board } from "../../models/Board"
 import { withAuth } from "../../hoc/withAuth"
 import { FirebaseUser } from "../../models/FirebaseUser"
 import { BoardsFacade } from "../../facades/BoardsFacade"
+import { BOARD_ROLES } from "../../constants/firebase"
 
 type BoardCreateProps = {
   authUser: FirebaseUser,
@@ -11,6 +12,7 @@ type BoardCreateProps = {
 
 type BoardCreateState = {
   name: string,
+  shareToEmails: Array<string>,
 }
 
 class BoardCreate extends React.PureComponent<
@@ -21,18 +23,59 @@ class BoardCreate extends React.PureComponent<
 
   state = {
     name: "",
+    shareToEmails: [],
   }
 
   onChangeName = (event: SyntheticEvent<HTMLInputElement>) => {
     return this.setState({ name: event.currentTarget.value })
   }
 
+  addShareToEmail = (email: string) => {
+    //todo - verify if email has a correct format
+    return this.setState({
+      shareToEmails: [email, ...this.state.shareToEmails],
+    })
+  }
+
+  removeShareToEmail = (index: number) => {
+    const currentShareTo = this.state.shareToEmails
+    const shareToEmails: Array<string> = [
+      ...currentShareTo.slice(0, index),
+      ...currentShareTo.slice(index + 1, currentShareTo.length),
+    ]
+    this.setState({ shareToEmails })
+  }
+
+  handleKeyPress = (event: any) => {
+    if (event.key === "Enter") {
+      console.log("enter key pressed")
+      const email = event.currentTarget.value
+      this.addShareToEmail(email)
+      event.currentTarget.value = ""
+    }
+  }
+
   onSave = () => {
+    const shareToEmails: Array<string> = [...this.state.shareToEmails]
+    // Convert list of e-mails to "role" object.
+    // Currently we only support "idea_editor" role.
+    const roles = shareToEmails.reduce(
+      (acc: { [string]: string }, email: string) => {
+        acc[email] = BOARD_ROLES.EDITOR
+        return acc
+      },
+      {}
+    )
     const board = new Board({
       name: this.state.name,
       ownerId: this.props.authUser.uid,
+      roles,
     })
     BoardsFacade.createBoard(board)
+      .then(() => {
+        console.log("Added new board")
+      })
+      .catch((error) => console.error("Error adding board: ", error))
   }
 
   render() {
@@ -41,6 +84,15 @@ class BoardCreate extends React.PureComponent<
         <h2>Add board</h2>
         <p>Name:</p>
         <input onChange={this.onChangeName} />
+        <p>Share To:</p>
+        <input onKeyPress={this.handleKeyPress} />
+        <ul>
+          {this.state.shareToEmails.map((email: string, index: number) => (
+            <li key={email} onClick={() => this.removeShareToEmail(index)}>
+              {email}
+            </li>
+          ))}
+        </ul>
         <button onClick={this.onSave}>Save</button>
       </div>
     )
