453e438304236035f168d17cd9133852ca586f7e
Krzysztof Gaszyński <k.gaszynski@gmail.com>
2018-08-31 17:20:26 +0200

    Total score without gradient

--- src/components/IdeaView/IdeaViewTotalScore.js
+++ src/components/IdeaView/IdeaViewTotalScore.js
@@ -12,9 +12,7 @@ const IdeaViewTotalScore = (props: IdeaViewTotalScoreProps) => {
       <span className="IdeaViewTotalScore__Caption">
         <b>total score</b>
       </span>
-      <span className="IdeaViewTotalScore__Value text-color-gradient">
-        {props.value}
-      </span>
+      <span className="IdeaViewTotalScore__Value">{props.value}</span>
     </div>
   )
 }
