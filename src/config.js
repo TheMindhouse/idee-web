e4a7981c5674896261e3e6322da166d330a99f42
Krzysztof Gaszyński <k.gaszynski@gmail.com>
2018-09-04 17:25:13 +0200

    Use production DB

--- src/config.js
+++ src/config.js
@@ -1,13 +1,13 @@
 // @flow
 
-// const FIREBASE_PRODUCTION = {
-//   apiKey: "AIzaSyDudKhN_MabmGH5EUNv7DPmBZ4ptMhFVog",
-//   authDomain: "idee-app.firebaseapp.com",
-//   databaseURL: "https://idee-app.firebaseio.com",
-//   projectId: "idee-app",
-//   storageBucket: "idee-app.appspot.com",
-//   messagingSenderId: "394685923584",
-// }
+const FIREBASE_PRODUCTION = {
+  apiKey: "AIzaSyDudKhN_MabmGH5EUNv7DPmBZ4ptMhFVog",
+  authDomain: "idee-app.firebaseapp.com",
+  databaseURL: "https://idee-app.firebaseio.com",
+  projectId: "idee-app",
+  storageBucket: "idee-app.appspot.com",
+  messagingSenderId: "394685923584",
+}
 
 const FIREBASE_DEV = {
   apiKey: "AIzaSyAmsLWMStkiaNlh5NpLm3yYabQ5aC6WU6I",
@@ -18,9 +18,14 @@ const FIREBASE_DEV = {
   messagingSenderId: "854949127425",
 }
 
-console.log("NODE_ENV = ", process.env.NODE_ENV)
+console.log(
+  "REACT_APP_FIREBASE_CONFIG = ",
+  process.env.REACT_APP_FIREBASE_CONFIG
+)
 
 export const CONFIG = {
-  firebase: FIREBASE_DEV,
-  // process.env.NODE_ENV === "production" ? FIREBASE_PRODUCTION : FIREBASE_DEV,
+  firebase:
+    process.env.REACT_APP_FIREBASE_CONFIG === "production"
+      ? FIREBASE_PRODUCTION
+      : FIREBASE_DEV,
 }
